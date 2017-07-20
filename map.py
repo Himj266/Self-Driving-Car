60c52e016f4fb9ff3e9bb00fd832fa77fe7fab0b
Ujjwal <ujjwalupadhyay8@gmail.com>
2017-07-20 15:46:32 +0530

    Map initialization

--- map.py
+++ map.py
@@ -2,10 +2,23 @@
 # Whole Game
 # Game avoiding obstacles in game
 
+# Importing required libraries
+import numpy as np
+
 # import deep q network from brain or ai
 from ai import Dqn
 
+# Brain of Self driving car initializing, its a neural network
 brain = Dqn(5,3,0.9)    # arguements: inputs, nb_action, gamma(discount factor)
 action2rotation = [0, 20, -20]  # All actions 
 last_reward = 0     # Reward at this time
 score = []      # Rewards onto the sliding windows
+
+# Initializing the map
+first_update = True
+def init():
+    global sand
+    global goal_x
+    global goal_y
+    global first_update
+    sand = np.zeros
