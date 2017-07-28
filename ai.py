a662b8992f5d1fc8eb4aeaee12e4d52291b2073a
Ujjwal <ujjwalupadhyay8@gmail.com>
2017-07-28 17:56:50 +0530

    Starting ai

--- ai.py
+++ ai.py
@@ -3,3 +3,19 @@
 # AI for self driving car
 
 # Importing libraries
+
+# Creating the architecture of the Neural Network
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+    
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
